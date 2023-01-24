df55db7e12bfa9f44ec872b2810a5bd1344c562a
HOP3N <pankoff97@gmail.com>
2023-01-24 10:34:52 +0200

    trying to push it right

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,6 +3,11 @@ import { galleryItems } from './gallery-items.js';
 
 console.log(galleryItems);
 
+
+import SimpleLightbox from 'simplelightbox';
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 const gallery = document.querySelector('.gallery');
 const items = [];
 
